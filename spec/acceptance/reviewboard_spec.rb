fdfcfe3c5e5f23f4556436808ba8f45ef4ab85f4
Scott Wales <scott.wales@unimelb.edu.au>
2014-04-03 17:36:52 +1100

    Test install

--- spec/acceptance/reviewboard_spec.rb
+++ spec/acceptance/reviewboard_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper_acceptance'
 
 describe 'reviewboard' do
+    it 'installs cleanly' do
+        pp = <<-EOS.unindent
+            class {'reviewboard':}
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes  => true)
+    end
 end
